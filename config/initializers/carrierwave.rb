a1bbe8aed551a234baa323b8add161fd3d41ae07
Yuji <rions.garden@icloud.com>
2017-12-24 11:27:37 +0900

    gitignore

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,9 +13,10 @@ CarrierWave.configure do |config|
   case Rails.env
   when 'production'
     config.fog_directory = '本番環境用のバケット名'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+    config.asset_host = 'https://s3.console.aws.amazon.com/s3/buckets/achieve-production-yuji91
+'
   when 'development'
     config.fog_directory = '開発環境用のバケット名'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+    config.asset_host = 'https://s3.console.aws.amazon.com/s3/buckets/achieve-development-yuji91
   end
 end
